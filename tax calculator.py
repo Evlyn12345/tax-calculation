7635019f1db025e23ff4a0c2d345743af2753eb6
Evlyn12345 <117650381+Evlyn12345@users.noreply.github.com>
2022-11-23 12:29:40 +0300

    Add files via upload

--- tax calculator.py
+++ tax calculator.py
@@ -1,11 +1,19 @@
+
 income = float(input("what is your income: "))
 
 if income <= 2820000:
     print("you have no tax")
 elif 2820000 < income <= 4020000:
-    new_income =(income - (income*0.9))
+    new_income = (income - (income*0.9))
     print("your tax amount to be paid is", new_income)
-else:
-    4020000 < income <= 4820000
+elif 4020000 < income <= 4820000:
     tax_amount = (income-(income*0.8) + 120000)
-    print("your will pay amount", tax_amount, "for your tax")
+    print("you will pay amount", tax_amount, "for your tax")
+elif 4920000 < income >= 120000000:
+    tax_amount = (income-((income*0.7) + 300000))
+    print("tax_amount")
+else:
+    income > 120000000
+    tax_amount = (income-((income*0.7) + 0.9 + 300000))
+    print("you are rich to be paying", tax_amount, "as your tax")
+
